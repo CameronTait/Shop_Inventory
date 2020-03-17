306d5cb8b9ac8ea0577203083805c4958ce0cac5
CameronTait <cameron-tait@hotmail.com>
2020-03-17 15:06:37 +0000

    can render product and customers@

--- app.rb
+++ app.rb
@@ -2,7 +2,6 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative('controllers/products_controller')
 require_relative('controllers/customers_controller')
-require_relative('controllers/subscriptions_controller')
 
 get '/' do
   erb( :index )
--- controllers/customers_controller.rb
+++ controllers/customers_controller.rb
@@ -2,3 +2,8 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( '../models/customer.rb' )
 also_reload( '../models/*' )
+
+get '/customers' do
+  @customers = Customer.all()
+  erb (:"customers/index")
+end
